c18ffc737cbefcb1cac2f4378f157c790dbfa191
GeovanyArdon <00118618@uca.edu.sv>
2022-11-10 13:01:52 -0600

    :sparkles: [Add] añadiendo en el modulo de inventario el mantenimiento de agregar, consultar y filtrar

--- src/components/InventarioComponents/InventarioCard.jsx
+++ src/components/InventarioComponents/InventarioCard.jsx
@@ -1,76 +1,106 @@
-import React from 'react';
-import { Box, Button, Card, CardActions, CardContent,CardMedia,TextField,Typography } from '@mui/material';
+import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 //Styled Components
 const StyledCard = {
-    display: 'flex',
-    flexDirection:"column",
-    alignItems:"center",
-    justifyContent:'center',
-    borderRadius: '20px',
-}
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "20px",
+};
 
 const StyledBox = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems:'center',
-    width:'100%',
-}
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+};
 
 const StyledInput = {
-    textAlign:'center'
+  textAlign: "center",
+};
+
+const StyleTypography = {
+  width:"90%",
+  marginLeft:'20px',
+  marginRight:'20px',
+  textOverflow: "ellipsis",
+  overflow: "hidden",
+  whiteSpace: "nowrap"
 }
 
-
-function InventarioCard({title, cantidad, precio, img}) {
-
-
-    //Handler Options
-    function showOptions(){
-        console.log('click');
-    }
-    return (
-        <Card sx={StyledCard}>
-            <Box sx={StyledBox}>
-                <Typography m="0.3em" component='div' variant="h4" align='center' color="text.primary">
-                    {title}
-                </Typography>
-                <CardMedia
-                    component="img"
-                    height="180"
-                    image={img}
-                    alt="mani"
-                />
-                    <CardContent>
-                        <Typography component='div' variant="h6" align='center' color="text.primary">
-                            Cantidad:
-                        </Typography>
-                        <TextField
-                            inputProps={{style: StyledInput}}
-                            fullWidth
-                            hiddenLabel
-                            defaultValue={cantidad}
-                            variant="filled"
-                            size="small"
-                        />
-                        <Typography component='div' variant="h6" align='center' color="text.primary">
-                            Precio:
-                        </Typography>
-                        <TextField
-                            inputProps={{style: StyledInput}}
-                            fullWidth
-                            hiddenLabel
-                            defaultValue={precio}
-                            variant="filled"
-                            size="small"
-                        />
-                    </CardContent>
-                <CardActions>
-                    <Button color="secondary" variant='outlined' size="medium">Actualizar</Button>
-                </CardActions>
-            </Box>
-        </Card>
-     );
+function InventarioCard({ title, cantidad, precio, img }) {
+  //Handler Options
+  function showOptions() {
+    console.log("click");
+  }
+  return (
+    <Card sx={StyledCard}>
+      <Box sx={StyledBox}>
+        <Typography
+          sx={StyleTypography}
+          noWrap
+          mt="0.3em"
+          component="div"
+          variant="h4"
+          align="center"
+          color="text.primary"
+        >
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography
+            component="div"
+            variant="h6"
+            align="center"
+            color="text.primary"
+          >
+            Cantidad:
+          </Typography>
+          <TextField
+            inputProps={{ style: StyledInput }}
+            fullWidth
+            disabled
+            hiddenLabel
+            value={cantidad}
+            variant="filled"
+            size="small"
+          />
+          <Typography
+            component="div"
+            variant="h6"
+            align="center"
+            color="text.primary"
+          >
+            Precio:
+          </Typography>
+          <TextField
+            inputProps={{ style: StyledInput }}
+            fullWidth
+            disabled
+            hiddenLabel
+            value={precio}
+            variant="filled"
+            size="small"
+          />
+        </CardContent>
+        <CardActions>
+          <Button color="secondary" variant="outlined" size="medium">
+            Actualizar
+          </Button>
+        </CardActions>
+      </Box>
+    </Card>
+  );
 }
 
-export {InventarioCard};
+export { InventarioCard };
